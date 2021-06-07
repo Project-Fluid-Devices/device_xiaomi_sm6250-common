f63b3eb3325e9e5b38aff5ae8aeff05f03cb2a51
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-06-07 14:56:18 +0200

    sm6250-common: Build missing BT packages
    * Needed because of new blobs from BSP.
    
    Signed-off-by: Alex Winkowski <dereference23@outlook.com>

--- common.mk
+++ common.mk
@@ -17,6 +17,10 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 PRODUCT_PACKAGES += \
     fs_config_files
 
+# ANT+
+PRODUCT_PACKAGES += \
+    com.dsi.ant@1.0.vendor
+
 # Apex
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
@@ -88,12 +92,14 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
+    libbluetooth_audio_session \
     libbthost_if \
     BluetoothQti
 
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    com.qualcomm.qti.bluetooth_audio@1.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
