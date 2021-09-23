80c5f8501123a55d19b722bae9de9d1e9854c244
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-23 16:49:09 +0200

    sm6250-common: Move to common Xiaomi light AIDL
    Change-Id: I00494cd392d19abd97cd65aeb4ec4bd9c9b11352

--- atoll.mk
+++ atoll.mk
@@ -211,7 +211,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.lights-service.xiaomi_atoll
+    android.hardware.light-service.xiaomi
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
